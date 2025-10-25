d992c9c6775aff8b850eafc721b015e6534ea2d0
lvmendess <liviamendes1703@gmail.com>
2025-10-25 14:33:52 -0300

    first commit

---
+++ src/main/java/com/vigiaenchente/ve/VeApplication.java
@@ -0,0 +1,13 @@
+package com.vigiaenchente.ve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vigiaenchente/ve/VeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigiaenchente.ve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
